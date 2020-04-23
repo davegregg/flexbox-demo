1d1f0a8db9336bbd430054662c7f5e0b65a41290
davegregg <davegregg@gmail.com>
2020-04-23 13:07:28 -0400

    Update index.html & onload.js with new solutions stylesheet filename.

--- scripts/onload.js
+++ scripts/onload.js
@@ -1,14 +1,14 @@
 /*
   You'll notice, first of all, that in the HEAD
   section of our HTML file, there is a LINK tag for
-  `.solutions.css` – but it has a DISABLED attribute,
+  `solutions.css` – but it has a DISABLED attribute,
   which will prevent it from loading initially, when
   the browser reads the HTML.
 
   The below JavaScript will check if this page is
   being hosted on Github.io, and if so, will unload
   the `flex-containers.css` stylesheet, and load the
-  `.solutions.css` stylesheet in its place.
+  `solutions.css` stylesheet in its place.
 
   This way, when you're viewing it over the Web, you
   can see the already-written solutions, but if you're
